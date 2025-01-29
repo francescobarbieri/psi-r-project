4e88634de5d2ae5b0f27fcbbbf32f1bc0fe5605d
Francesco Barbieri <fbarbieri.business@gmail.com>
2025-01-29 19:07:58 +0100

    parsing file

--- main.R
+++ main.R
@@ -1,3 +1,9 @@
-data <- read.table("revenues2023.txt", header=FALSE, sep="\t")
-vendite <- data[, 1:9]
-ricavi <- data[, 10:ncol(data)]
+alldata <- read.table("revenues2023.txt", header = FALSE, sep = "\t")
+# Removing the first column, defect in parsing txt file
+alldata <- alldata[, -1]
+vendite <- alldata[, 1:9]
+ricavi <- alldata[, c(1, 10:ncol(alldata))]
+names(vendite) <- c("settimana", "trekking", "mtb", "strada", "abbigliamento", "accessori", "componenti", "componenti-assistenza", "servizi")
+names(ricavi) <- c("settimana", "trekking", "mtb", "strada", "abbigliamento", "accessori", "componenti", "servizi")
+str(vendite)
+str(ricavi)
